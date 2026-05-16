36e7f499533f85bfb9b97fc8bff8e8e11011106c
Christian Relf <christianjamesrelf@gmail.com>
2026-05-17 00:46:04 +0100

    Update SignInPage.tsx

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -2,6 +2,21 @@ import { SignIn } from "@clerk/clerk-react"
 
 export default function SignInPage() {
   return (
+    <div
+      className="flex flex-col min-h-[calc(100vh-4rem)] items-center justify-center px-4 py-12"
+      style={{
+        backgroundImage: "url('/polymartbackground.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <img
+        src="/polymartlogoblack.png"
+        alt="Polymart"
+        className="h-10 mb-8 select-none"
+        draggable={false}
+      />
       <SignIn
         routing="virtual"
         signUpUrl="/#/sign-up"
